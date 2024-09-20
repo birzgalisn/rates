a529eec8f5958b0c2ad387ceeed511375b5f8a2b
Niks Birzgalis <hello@niksbirzgalis.com>
2024-09-20 13:15:47 +0300

    fix(apps/web): use correct sort key for rates search params

--- apps/web/app/query-options/rates/create.ts
+++ apps/web/app/query-options/rates/create.ts
@@ -4,19 +4,19 @@ import { Rate } from '@repo/api/rates/entities/rate.entity';
 
 export function createRatesOptions(searchParams: URLSearchParams) {
   const rate = searchParams.get('rate') ?? 'usd';
-  const order = searchParams.get('order') ?? 'desc';
+  const sort = searchParams.get('sort') ?? 'desc';
   const page = searchParams.get('page') ?? '1';
   const perPage = searchParams.get('perPage') ?? '10';
 
   const url =
     `${process.env.NEXT_PUBLIC_API_HOST}/rates` +
     `?rate=${rate}` +
-    `&order=${order}` +
+    `&sort=${sort}` +
     `&page=${page}` +
     `&perPage=${perPage}`;
 
   return queryOptions<Rate>({
-    queryKey: ['rates', rate, order, page, perPage],
+    queryKey: ['rates', rate, sort, page, perPage],
     queryFn: async () => {
       const res = await fetch(url);
       return res.json();
