fa1f259a09f5f2c063faf87390b4301aa8497adf
Niks Birzgalis <hello@niksbirzgalis.com>
2024-10-04 18:02:28 +0300

    feat(apps/cron): separate cron into a standalone app from api

--- apps/cron/src/app.module.ts
+++ apps/cron/src/app.module.ts
@@ -2,22 +2,24 @@ import { redisStore } from 'cache-manager-redis-yet';
 
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { CacheModule } from '@nestjs/cache-manager';
 
 import { AppService } from '~/app.service';
 import { AppController } from '~/app.controller';
 
-import { RatesModule } from '~/rates/rates.module';
+import { CsvModule } from '~/csv/csv.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
     CacheModule.register({
       isGlobal: true,
       store: redisStore,
       url: process.env.CACHE_URL,
     }),
-    RatesModule,
+    CsvModule,
   ],
   controllers: [AppController],
   providers: [AppService],
